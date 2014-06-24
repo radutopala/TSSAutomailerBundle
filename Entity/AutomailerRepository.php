101480c4d27ce22b7797ab845ae5bd8e84a759b3
89031007131 <sozidanierf@gmail.com>
2014-06-24 19:09:53 +0300

    Update AutomailerRepository.php

--- Entity/AutomailerRepository.php
+++ Entity/AutomailerRepository.php
@@ -12,9 +12,9 @@ class AutomailerRepository extends EntityRepository
     public function findNext($limit)
     {
         $query = $this->getEntityManager()->createQuery("SELECT am FROM TSSAutomailerBundle:Automailer am WHERE am.isSent = :is_sent AND am.isFailed = :is_failed AND am.isSending = :is_sending ORDER BY am.createdAt ASC")
-				->setParameter('is_sent', false)
-				->setParameter('is_failed', false)
-				->setParameter('is_sending', false)
+				->setParameter('is_sent', 0)
+				->setParameter('is_failed', 0)
+				->setParameter('is_sending', 0)
 				->setMaxResults($limit);
 		return $query->getResult();
     }
